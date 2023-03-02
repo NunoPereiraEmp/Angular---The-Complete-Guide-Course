44c14ac6fa02ed2a410214153ce7a6b9e36f07d9
NunoPereiraEmp <nuno.pereira@valeriushub.com>
2023-03-02 15:21:50 +0000

    teste2

--- my-first-app2/src/app/servers/servers.component.ts
+++ my-first-app2/src/app/servers/servers.component.ts
@@ -1,7 +1,15 @@
 import { Component, OnInit} from '@angular/core';
 
 @Component({
+  //podemos converter o selector component como um atributo
+  // app-servers  ficando desta forma [app-servers] fica assim  <div app-servers></div>
+  //ou .app-servers <div class="app-servers"></div>
+  //e basicamente em app.component.html pomos o app-server dentro de
+  //um div
+  //selector: '.app-servers',
+  //selector: '[app-servers]',
   selector: 'app-servers',
+  
    //podemos substituir o templateUrl para template para por
   //exemplo para escrever uma string de codigo html
  // template: '<app-server></app-server><app-server></app-server>',
