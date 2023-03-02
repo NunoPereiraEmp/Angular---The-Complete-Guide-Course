b8213553db1391ac2ea3277d4b8bd75995f56cb3
NunoPereiraEmp <nuno.pereira@valeriushub.com>
2023-03-02 16:23:52 +0000

    mais testes

---
+++ practicing-components/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
