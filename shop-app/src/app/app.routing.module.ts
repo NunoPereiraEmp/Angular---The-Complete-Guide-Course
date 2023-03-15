5646811ac27cc0da49ce75ea445234dec725eade
NunoPereiraEmp <nuno.pereira@valeriushub.com>
2023-03-15 14:55:17 +0000

    get id

--- shop-app/src/app/app.routing.module.ts
+++ shop-app/src/app/app.routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
@@ -22,9 +23,15 @@ const appRoutess: Routes = [
             {
                 path: '', component: RecipeStartComponent
             },
+            {
+                path: 'new', component: RecipeEditComponent
+            },
             {//id do recipe item
                 path: ':id', component: RecipeDetailComponent
             },
+            {
+                path: ':id/edit', component: RecipeEditComponent
+            }
             
         ]
     },
